eb99d01fb6be37b12202a7227cf31bd0877debf6
Tomas Arjovsky <t.arjovsky@gmail.com>
2014-05-03 13:55:57 -0300

    cambio sintaxis de comentarios en lineales

--- lineales.m
+++ lineales.m
@@ -1,15 +1,15 @@
-#La idea es pasarle una matriz ampliada (R^(n*(n+1))). Lo que devuelva cada metodo debe ser un vector de R^n, solucion del sistema. Por esto, para todos los métodos se supone un sistema representado por una matriz cuadrada.
+%La idea es pasarle una matriz ampliada (R^(n*(n+1))). Lo que devuelva cada metodo debe ser un vector de R^n, solucion del sistema. Por esto, para todos los métodos se supone un sistema representado por una matriz cuadrada.
 
-#A los iterativos se les pasa la cantidad de iteraciones. Despues sera una condicion de corte.
+%A los iterativos se les pasa la cantidad de iteraciones. Despues sera una condicion de corte.
 
-#Directos. Se recibe matriz diagonal ampliada. Resolución de Lower (diagonal inferior).
+%Directos. Se recibe matriz diagonal ampliada. Resolución de Lower (diagonal inferior).
 
 function x = despejarInferior(L)
 	x = 0;
 	n = rows(L);
 	for i = [1:n]
 		s = sum( L( i,[1: (i-1)] ) .* x([1:(i-1)]) );
-		#Los b están incluidos en L. b(i) = L(i,end)
+		%Los b están incluidos en L. b(i) = L(i,end)
 		x(i) = (L(i,n + 1) - s)/L(i,i);
 	endfor
 endfunction
@@ -23,24 +23,34 @@ function x = despejarSuperior(U)
 	endfor
 endfunction
 
-#Con pivote parcial.
+%Con pivote parcial.
 function LU = factorizarLU(M)
-	#Eliminación de Gauss, pero aprovecha los inferiores para guardar los multiplicadores.
+	%Eliminación de Gauss, pero aprovecha los inferiores para guardar los multiplicadores.
 	n = rows(M);
 	for i = [2:n]
 		huboCambios = false;
-		#pivoteo parcial. Buscar forma de no intercambiar filas, sino guardar los cambios.
+		%pivoteo parcial. Buscar forma de no intercambiar filas, sino guardar los cambios.
+		posMaximo = [i,i];
+		for j = [i, rows(M)]
+			if ( M(j,i) < m)
+				posMaximo %terminar
+			
+		endfor
 		
-		
-		#eliminación
+		%eliminación
 		if ( M(i, i - 1) != 0 )
 			huboCambios  = true;
 			multiplicador  = M(i,i-1)/M(i-1,i-1);
-			#guardo multiplicador en la inferior.
+			%guardo multiplicador en la inferior.
 			M(i,i - 1) = multiplicador;
-			M(i,[i:n]) -= M(i - 1,[i:n]) * multiplicador;
+			M(i,[i:n + 1]) -= M(i - 1,[i:n + 1]) * multiplicador;
 		endif
 	endfor	
+endfunction
+
+function x = Gauss(M)
+	 LU = factorizarLU(M);
+	 x = despejarSuperior(LU);
 endfunction
 
 L = [1,0,0,4;1,1,0,7;1,1,1,8];
