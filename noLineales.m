cfef1c6d4c2e92efeab65c70aaf10b1ca55af81a
Tomas Arjovsky <t.arjovsky@gmail.com>
2014-05-03 13:53:50 -0300

    cambio sintaxis de comentarios

--- noLineales.m
+++ noLineales.m
@@ -1,4 +1,4 @@
-#Empezamos con un polinomio de ejemplo. Luego se hará una entrada más general.
+%Empezamos con un polinomio de ejemplo. Luego se hará una entrada más general.
 function r = polinomio (coefs,x)
 	res = 0.0;
 	i = 0;
@@ -9,16 +9,16 @@ function r = polinomio (coefs,x)
 	r = res;
 endfunction
 
-#Segun los coeficientes crea un polinomio listo para evaluar
+%Segun los coeficientes crea un polinomio listo para evaluar
 function p = polEval(coeficientes)
 	p = @(a) polinomio(coeficientes,a);
 endfunction
 	
-#Funcion principal de biseccion. Dada una funcion de evaluación y un rango([inicio,fin]), devuelve una raiz.
+%Funcion principal de biseccion. Dada una funcion de evaluación y un rango([inicio,fin]), devuelve una raiz.
 function raiz = biseccion(f,rango)
 	a = rango(1);
 	b = rango(2);
-	#comienzo con una cantidad arbitraria de iteraciones, luego hago algun criterio mejor.
+	%comienzo con una cantidad arbitraria de iteraciones, luego hago algun criterio mejor.
 	for i = [1:5]
 		m = (a + b)/2;
 		if (f(m)*f(a) < 0)
@@ -33,9 +33,9 @@ endfunction
 function raiz = RegulaFalsi(f,rango)
 	a = rango(1);
 	b = rango(2);
-	#comienzo con una cantidad arbitraria de iteraciones, luego hago algun criterio mejor.
+	%comienzo con una cantidad arbitraria de iteraciones, luego hago algun criterio mejor.
 	for i = [1:5]
-		#esto es lo unico que cambia en RS. El punto medio se busca de otra forma.
+		%esto es lo unico que cambia en RS. El punto medio se busca de otra forma.
 		m = a - f(a)*((b-a)/(f(b)-f(a)));
 		if (f(m)*f(a) < 0)
 			b = m;
@@ -46,9 +46,9 @@ function raiz = RegulaFalsi(f,rango)
 	raiz = m;
 endfunction
 
-#Se obtienen los coeficientes de un
+%Se obtienen los coeficientes de un
 function nc = coeficientesDerivados(coefs)
-	#saco el primer coeficiente.
+	%saco el primer coeficiente.
 	res = coefs(2:length(coefs));
 	if (!isempty(res))
 		for (i = 1:length(res))
@@ -60,12 +60,12 @@ function nc = coeficientesDerivados(coefs)
 	nc = res;
 endfunction
 
-#Se pasan los coeficientes de un polinomio y se obtiene el polinomio derivado.
+%Se pasan los coeficientes de un polinomio y se obtiene el polinomio derivado.
 function pd = PolinomioDerivado(coefs)
 	pd = @(x) polinomio(coeficientesDerivados(coefs),x);
 endfunction
 
-#
+%
 function pf = NewtonRaphson(f,df,seed)
 	x = seed;
 	for i  = 1:5
@@ -77,8 +77,8 @@ endfunction
 pol = polEval([-2,0,1]);
 dpol = PolinomioDerivado([-2,0,1])
 
-#fplot(pol,[-5,5])
-#fplot(dpol,[-5,5])
+%fplot(pol,[-5,5])
+%fplot(dpol,[-5,5])
 
 r = biseccion(pol,[0,10])
 r = RegulaFalsi(pol,[0,10])
